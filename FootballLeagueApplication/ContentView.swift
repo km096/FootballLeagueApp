27e042a329ae36f8b9de8e44727951ea0354ad70
Kareem <kareem.khaled1000@gmail.com>
2024-06-09 16:51:56 +0300

    Initial Commit

---
+++ FootballLeagueApplication/ContentView.swift
@@ -0,0 +1,23 @@
+//FootballLeagueApplication
+//ContentView.swift
+
+//Created by: Kareem on 6/9/24                      
+//Copyright (c) 2023 Kareem
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FootballLeagueApplication/FootballLeagueApplicationApp.swift
@@ -0,0 +1,16 @@
+//FootballLeagueApplication
+//FootballLeagueApplicationApp.swift
+
+//Created by: Kareem on 6/9/24                      
+//Copyright (c) 2023 Kareem
+
+import SwiftUI
+
+@main
+struct FootballLeagueApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
